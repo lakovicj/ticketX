8cb26f95d8b181b1048bdbb1fc709cb895c426ca
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-08-03 19:45:20 +0200

    init

---
+++ ticketX/src/main/java/ftn/ticketX/TicketXApplication.java
@@ -0,0 +1,13 @@
+package ftn.ticketX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketXApplication.class, args);
+	}
+
+}
---
+++ ticketX/src/test/java/ftn/ticketX/TicketXApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.ticketX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
